da365b2885353dd3d9a2d5c9997db526a7a67c26
increpare <analytic@gmail.com>
2025-09-16 19:49:42 +0200

    centralized a lot of stuff

--- flickgame_vanilla.js
+++ flickgame_vanilla.js
@@ -1,42 +1,3 @@
-// Flick Game JavaScript Library
-// Extracted shared functions from HTML files
-
-// Global variables
-var gameState;
-
-// Helper function to get URL parameters
-function getParameterByName(name) {
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-// Helper function to strip http/https from URLs
-function strip_http(url) {
-   url = url.replace(/^https?:\/\//,'');
-   return url;
-}
-
-// Helper function to qualify URLs
-function qualifyURL(url) {
-    var aurl = document.createElement('a');
-    aurl.href = url;
-    return aurl.href;
-}
-
-// RLE decode function
-function RLE_decode(encoded) {
-    var output = "";
-    for (var i=0;i<encoded.length;i+=2) {
-      var count = encoded[i];
-      var ch = encoded[i+1];
-      for (var j=0;j<count;j++){
-        output+=ch;
-      }
-    }
-    return output;
-}
-
 // Color palettes
 var palettes = {
   antiquity16: [
@@ -363,140 +324,32 @@ var palettes = {
 
 // Load state function
 function loadState(code) {
-    gameState = JSON.parse(code);
-    if (gameState.palette_name === undefined) {
-        console.log("palette_name not defined, setting to dawnbringer-16");
-        gameState.palette_name = "dawnbringer-16";
-    }
-    if (gameState.background_color === undefined) {
-        console.log("background_color not defined, setting to #000000");
-        gameState.background_color = "#000000";
-    }
-    if (gameState.foreground_color === undefined) {
-        console.log("foreground_color not defined, setting to #ffffff");
-        gameState.foreground_color = "#ffffff";
-    }
-    colorPalette = palettes[gameState.palette_name];
+  gameState = JSON.parse(code);
+  if (gameState.palette_name === undefined) {
+      console.log("palette_name not defined, setting to dawnbringer-16");
+      gameState.palette_name = "dawnbringer-16";
+  }
+  if (gameState.background_color === undefined) {
+      console.log("background_color not defined, setting to #000000");
+      gameState.background_color = "#000000";
+  }
+  if (gameState.foreground_color === undefined) {
+      console.log("foreground_color not defined, setting to #ffffff");
+      gameState.foreground_color = "#ffffff";
+  }
+  colorPalette = palettes[gameState.palette_name];
 
-    // Set background color
-    document.documentElement.style.setProperty('--background-color', gameState.background_color);
-    document.documentElement.style.setProperty('--foreground-color', gameState.foreground_color);
+  // Set background color
+  document.documentElement.style.setProperty('--background-color', gameState.background_color);
+  document.documentElement.style.setProperty('--foreground-color', gameState.foreground_color);
 
-    var homepage = gameState.gameLink;
-    var homepageLink = document.getElementById("homeLink");
-    if (homepageLink) {
-        homepageLink.innerHTML = strip_http(homepage);
-        if (!homepage.match(/^https?:\/\//)) {
-            homepage = "http://" + homepage;
-        }
-        homepageLink.href = homepage;
-    }
-}
-
-// Generic getData function that can be used by both player and editor
-function getData(options) {
-    options = options || {};
-    var onSuccess = options.onSuccess || function(code) {
-        // Default behavior for play.html
-        loadState(code);
-        
-        var homepage = gameState.gameLink;
-        var homepageLink = document.getElementById("homeLink");
-        if (homepageLink) {
-            homepageLink.innerHTML = strip_http(homepage);
-            if (!homepage.match(/^https?:\/\//)) {
-                homepage = "http://" + homepage;
-            }
-            homepageLink.href = homepage;
-        }
-  
-        var hacklink = document.getElementById("hackLink");
-        if (hacklink) {
-            var url = "index.html?p=" + id;
-            url = qualifyURL(url);
-            hacklink.href = url;
-            hacklink.innerHTML = "&sdotb; edit";
-        }
-        
-        // Call renderImages if it exists
-        if (typeof renderImages === 'function') {
-            renderImages();
-        }
-    };
-    var onError = options.onError || function(message) {
-        console.log(message);
-    };
-    var onEmbedded = options.onEmbedded || function(code) {
-        // Default behavior for embedded data
-        loadState(code);
-        
-        var homepage = gameState.gameLink;
-        var homepageLink = document.getElementById("homeLink");
-        if (homepageLink) {
-            homepageLink.innerHTML = strip_http(homepage);
-            if (!homepage.match(/^https?:\/\//)) {
-                homepage = "http://" + homepage;
-            }
-            homepageLink.href = homepage;
-        }
-    };
-
-    // Handle embedded data
-    if (typeof embeddedDat !== 'undefined' && embeddedDat[0] !== '_') {
-        embeddedDat = decodeURI(embeddedDat);
-        onEmbedded(embeddedDat);
-        return;
-    }
-
-    // Get ID from URL parameter
-    var id = getParameterByName("p").replace(/[\\\/]/, "");
-    if (id === null || id.length === 0) {
-        onError("No ID specified in URL.");
-        return false;
-    }
-
-    // Choose URL based on whether to use proxy
-    var url = "https://ded.increpare.com/cgi-bin/gist_proxy.py?id=" + id;
-    
-    // this instead to use the GitHub API directly (which is broken because of https://github.com/increpare/flickgame/issues/77 )
-    // url = 'https://api.github.com/gists/' + id;
-    
-    var httpClient = new XMLHttpRequest();
-    httpClient.open('GET', url);
-    httpClient.onreadystatechange = function() {
-        if (httpClient.readyState != 4) {
-            return;
-        }
-
-        var code;        
-        
-        // Handle proxy response
-        if (httpClient.status === 200) {
-            try {
-                var response = JSON.parse(httpClient.responseText);
-                if (response.error) {
-                    onError("Proxy error: " + response.error);
-                    return;
-                } else if (response.content) {
-                    code = response.content;
-                } else {
-                    onError("Invalid proxy response");
-                    return;
-                }
-            } catch (e) {
-                onError("Failed to parse proxy response");
-                return;
-            }
-        } else {
-            onError("Proxy request failed with status " + httpClient.status);
-            return;
-        }
-    
-
-        // Call success callback with the loaded code
-        onSuccess(code);
-    }
-    
-    httpClient.send();
-    return true;
+  var homepage = gameState.gameLink;
+  var homepageLink = document.getElementById("homeLink");
+  if (homepageLink) {
+      homepageLink.innerHTML = strip_http(homepage);
+      if (!homepage.match(/^https?:\/\//)) {
+          homepage = "http://" + homepage;
+      }
+      homepageLink.href = homepage;
+  }
 }
